4a678b7237e64aa5a58f945e9f0912b9e4f9c854
mohit0268 <mohit0268@users.noreply.github.com>
2021-07-23 18:46:32 +0530

    add json format file for translation

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const textInput=document.querySelector("#text-Input")
 const btnTranslate=document.querySelector("#btn-Translate");
 const output=document.querySelector("#output")
 
+const serverUrl="https://minions-translation.mohit0268.repl.co/Translate/minion.json";
 
-function clickHandler(){}
+function urlTranslation(text){
+    return serverUrl +"?"+ "text=" +  text;
+}
+
+function clickHandler(){
+ 
+
+
  
 btnTranslate.addEventListener("click",clickHandler());
