8fffbfcafea13061268d210d223aa493e14a424b
mohit0268 <mohit0268@users.noreply.github.com>
2021-07-23 18:45:46 +0530

    call function and perform event

---
+++ app.js
@@ -0,0 +1,8 @@
+const textInput=document.querySelector("#text-Input")
+const btnTranslate=document.querySelector("#btn-Translate");
+const output=document.querySelector("#output")
+
+
+function clickHandler(){}
+ 
+btnTranslate.addEventListener("click",clickHandler());
